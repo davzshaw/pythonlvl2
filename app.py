7112ddeb83d6610a2fc248aba350867d09235738
David <juandis0246@gmail.com>
2024-10-01 23:33:01 -0500

    NEw ports

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 @app.route('/home')
 def home():
-  return render_template('home.html')
+    return render_template('home.html')
+
 @app.route('/about')
 def about():
-  return render_template('about.html')
+    return render_template('about.html')
+
 @app.route('/contact')
 def contact():
-  return render_template('contact.html')
+    return render_template('contact.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
